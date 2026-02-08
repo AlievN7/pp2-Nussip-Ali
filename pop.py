9b409cff2144fb81fae830bd9e8038b92c2b5bab
Nusip Ali <alinusip176@gmail.com>
2026-02-08 19:00:47 +0500

    Complete Practice 3: Python functions, lambda, classes, and inheritance examples

--- pop.py
+++ pop.py
@@ -1,2 +1,22 @@
-print(bool("Hello"))
-print(bool(15))
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def move(self, new_x, new_y):
+        self.x = new_x
+        self.y = new_y
+    def show(self):
+        return self.x, self.y
+    def dist(self, other):
+        return (((other.x - self.x)**2 + (other.y - self.y)**2)**0.5)
+    
+x1, y1 = map(int, input().split())
+x2, y2 = map(int, input().split())
+x3, y3 = map(int, input().split())
+n1 = Point(x1, y1)
+print(n1.show())
+n2 = Point(x2, y2)
+print(n2.show())
+n1.move(n2.x, n2.y)
+n3 = Point(x3, y3)
+print(f"{n2.dist(n3):.2f}")
--- pp2.py
+++ pp2.py
@@ -1,15 +1,54 @@
-n = int(input())
-m = {}
-out = []
-for i in range(n):
-    a = input().split()
-    if a[0] == "set":
-        m[a[1]] = a[2]
-    else:
-        if a[1] in m.keys():
-            out.append(m[a[1]])
-        else:
-            out.append("KE: no key " + a[1] + " found in the document")
+def sTOn(n):
+    x = ""
+    x1 = list(n)
+    x2 = ""
+    for i in range(0, len(x1), 3):
+        if x1[i] != "-" and x1[i] != "+" and x1[i] != "*":
+            x2 += x1[i] + x1[i + 1] + x1[i + 2]
+            if x2 == "ONE":
+                x += "1"
+            elif x2 == "TWO":
+                x += "2"
+            elif x2 == "THR":
+                x += "3"
+            elif x2 == "FOU":
+                x += "4"
+            elif x2 == "FIV":
+                x += "5"
+            elif x2 == "SIX":
+                x += "6"
+            elif x2 == "SEV":
+                x += "7"
+            elif x2 == "EIG":
+                x += "8"
+            elif x2 == "NIN":
+                x += "9"
+    return x
 
-for _ in out:
-    print(_)
+def nTOs(n):
+    s = ""
+    while n != 0:
+        if n == 1:
+            s += "ONE"
+        elif n == 2:
+            s += "TWO"
+        elif n == 3:
+            s += "THR"
+        elif n == 4:
+            s += "FOU"
+        elif n == 5:
+            s += "FIV"
+        elif n == 6:
+            s += "SIX"
+        elif n == 7:
+            s += "SEV"
+        elif n == 8:
+            s += "EIG"
+        elif n == 9:
+            s += "NIN"
+        n //= 10
+    return s
+q = int(input())
+n = input()
+print(nTOs(q))
+print(sTOn(n))
